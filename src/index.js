6a761bf797c5d30898dc6c58ca3c2b7999a40332
Mariia Slobodian <marislobodian@gmail.com>
2020-03-19 23:53:01 +0200

    Initial page opening

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('Hello Sponge!');
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://www.behance.net', { waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2'] });
+    await page.screenshot({path: 'screenshots/example-desktop.png'});
+    await page.setViewport({width: 375, height: 812, isMobile: true});
+    await page.reload();
+    await page.screenshot({path: 'screenshots/example-mobile.png'});
+
+    await browser.close();
+})();
