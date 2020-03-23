5e174e3533d6faad12f64e91560962b0e868f883
Mariia Slobodian <marislobodian@gmail.com>
2020-03-23 21:48:33 +0200

    Extract function for making screenshots of web and mobile page view

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ console.log('Hello Sponge!');
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: false, devtools: true});
     const page = await browser.newPage();
     await page.goto('https://www.behance.net', { waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2'] });
-    await page.screenshot({path: 'screenshots/example-desktop.png'});
-    await page.setViewport({width: 375, height: 812, isMobile: true});
-    await page.reload();
-    await page.screenshot({path: 'screenshots/example-mobile.png'});
+    await makeDesktopAndMobileScreenshot(page, 'main-page-1');
 
     await browser.close();
 })();
+
+async function makeDesktopAndMobileScreenshot(page, name) {
+    await page.screenshot({path: `screenshots/${name}-desktop.png`});
+    await page.setViewport({width: 375, height: 812, isMobile: true});
+    await page.reload();
+    await page.screenshot({path: `screenshots/${name}-mobile.png`});
+}
