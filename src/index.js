d1fca3d9a0401d0c80403e8a537f9fb4b38646c8
Mariia Slobodian <marislobodian@gmail.com>
2020-03-23 23:03:25 +0200

    Add search function

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ console.log('Hello Sponge!');
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false, devtools: true});
+    const browser = await puppeteer.launch({headless: false, devtools: false});
     const page = await browser.newPage();
-    await page.goto('https://www.behance.net', { waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2'] });
+    await page.goto('https://www.behance.net', {waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']});
+    await openSearch(page, 'fluffy');
+
     await makeDesktopAndMobileScreenshot(page, 'main-page-1');
 
     await browser.close();
@@ -16,4 +18,17 @@ async function makeDesktopAndMobileScreenshot(page, name) {
     await page.setViewport({width: 375, height: 812, isMobile: true});
     await page.reload();
     await page.screenshot({path: `screenshots/${name}-mobile.png`});
+}
+
+async function openSearch(page, keyword) {
+    await Promise.all([
+        page.click('.rf-primary-nav__link--search'),
+        page.waitForNavigation({waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']}),
+    ]);
+    await page.type('input[type="search"]', keyword);
+
+    await Promise.all([
+       page.keyboard.press('Enter'),
+        page.waitForNavigation({waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']}),
+    ]);
 }
